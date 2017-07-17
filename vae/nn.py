83a9a23f9d35a67b5d8521435a29c2395f541cad
Lucas Deecke <ldeecke@gmail.com>
2017-07-17 12:21:27 +0200

    modified architecture, now involves convolutions

--- vae/nn.py
+++ vae/nn.py
@@ -12,11 +12,17 @@ class Encoder(nn.Module):
 		super(Encoder, self).__init__()
 		self.config = config
 		self.main = nn.Sequential(
-			nn.Linear(config.num_channels * config.img_size**2, config.h_dim),
+			nn.Conv2d(config.num_channels, 16, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.Conv2d(16, 32, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.Conv2d(32, 64, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.Conv2d(64, 32, 4, 2, 1, bias=True),
 			nn.ReLU()
 			)
-		self.linear_mu = nn.Linear(self.config.h_dim, self.config.z_dim)
-		self.linear_log_sigma_sq = nn.Linear(self.config.h_dim, self.config.z_dim)
+		self.linear_mu = nn.Linear(config.c_dim_flat, self.config.z_dim)
+		self.linear_log_sigma_sq = nn.Linear(config.c_dim_flat, self.config.z_dim)
 		self.reset_bias_and_weights()
 
 	def reset_bias_and_weights(self):
@@ -24,9 +30,13 @@ class Encoder(nn.Module):
 			if isinstance(m, nn.Linear):
 				m.weight.data.normal_(0.0, self.config.std)
 				m.bias.data.normal_(0.0, self.config.std)
+			if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+				m.weight.data.normal_(0.0, self.config.std)
+				m.bias.data.zero_()
 
 	def forward(self, input):
 		h = self.main(input)
+		h = h.resize(h.size(0), h.size(1) * h.size(2) * h.size(3))
 		return self.linear_mu(h), self.linear_log_sigma_sq(h)
 
 
@@ -37,10 +47,20 @@ class Decoder(nn.Module):
 	def __init__(self, config):
 		super(Decoder, self).__init__()
 		self.config = config
-		self.main = nn.Sequential(
-			nn.Linear(config.z_dim, config.h_dim),
+		self.main_1 = nn.Sequential(
+			nn.Linear(config.z_dim, config.c_dim_flat),
 			nn.ReLU(),
-			nn.Linear(config.h_dim, config.num_channels * config.img_size**2),
+			nn.Linear(config.c_dim_flat, config.c_dim_flat),
+			nn.ReLU()
+			)
+		self.main_2 = nn.Sequential(
+			nn.ConvTranspose2d(32, 64, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.ConvTranspose2d(32, 16, 4, 2, 1, bias=True),
+			nn.ReLU(),
+			nn.ConvTranspose2d(16, config.num_channels, 4, 2, 1, bias=True),
 			nn.Sigmoid()
 			)
 		self.reset_bias_and_weights()
@@ -50,9 +70,15 @@ class Decoder(nn.Module):
 			if isinstance(m, nn.Linear):
 				m.weight.data.normal_(0.0, self.config.std)
 				m.bias.data.normal_(0.0, self.config.std)
+			if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+				m.weight.data.normal_(0.0, self.config.std)
+				m.bias.data.zero_()
 
 	def forward(self, input):
-		return self.main(input)
+		h = self.main_1(input)
+		h = h.resize(input.size(0), *self.config.c_dim)
+		x = self.main_2(h)
+		return x
 
 
 class VAE(nn.Module):
